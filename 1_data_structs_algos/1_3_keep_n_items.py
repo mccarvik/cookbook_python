eb6763d9aabc09b5018cd5b0fe0f98faf3cce515
EC2 Default User <ec2-user@ip-172-31-91-31.ec2.internal>
2020-01-08 00:24:49 +0000

    going thru another section:

---
+++ 1_data_structs_algos/1_3_keep_n_items.py
@@ -0,0 +1,31 @@
+import pdb
+from collections import deque
+
+
+# yields the matching line along with previous N lines
+def search(lines, pattern, history=5):
+    # creates a queue that holds a max of "history" values
+    previous_lines = deque(maxlen = history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+        
+if __name__ == '__main__':
+    with open('somefile.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline,end='')
+            print(line,end='')
+            print('-'*20)
+
+# Deque max size is set, as the q extends, it removes older values
+q = deque(maxlen=3)
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+q.append(4)
+print(q)
+q.append(5)
+print(q)
